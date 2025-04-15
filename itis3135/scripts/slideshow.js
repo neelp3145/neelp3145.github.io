80dfa8f414d6fad0a8b9f90796c9eeaf57337823
neelp3145 <71790154+neelp3145@users.noreply.github.com>
2025-04-15 12:02:22 -0400

    change

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,10 +1,33 @@
 $(document).ready(function () {
-    $(".thumb").click(function () {
-        const newSrc = $(this).attr("src");
-        const newAlt = $(this).attr("alt");
+    const thumbnails = $(".thumb");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        const selectedThumb = thumbnails.eq(index);
+        const newSrc = selectedThumb.attr("src");
+        const newAlt = selectedThumb.attr("alt");
 
         $("#mainImage").attr("src", newSrc);
         $("#mainImage").attr("alt", newAlt);
         $("#caption").text(newAlt);
+        currentIndex = index;
+    }
+
+    // Thumbnail click
+    thumbnails.click(function () {
+        currentIndex = thumbnails.index(this);
+        showImage(currentIndex);
+    });
+
+    // Next button
+    $("#nextBtn").click(function () {
+        currentIndex = (currentIndex + 1) % thumbnails.length;
+        showImage(currentIndex);
+    });
+
+    // Prev button
+    $("#prevBtn").click(function () {
+        currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+        showImage(currentIndex);
     });
 });
